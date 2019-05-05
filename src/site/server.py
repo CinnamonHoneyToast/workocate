518bd0a536375eb51f795e3b289c082204e24534
GauravPK <gaurav.prabhu7@gmail.com>
2019-05-05 16:17:08 +0530

    Rename Flask to site + Add UI Image

---
+++ src/site/server.py
@@ -0,0 +1,11 @@
+# import the nessecary pieces from Flask
+from flask import Flask, render_template, request, jsonify, Response, request
+from flaskext.mysql import MySQL
+
+#Create the app object that will route our calls
+app = Flask(__name__)
+
+# Add a single endpoint that we can use for testing
+@app.route('/', methods = ['GET'])
+def home():
+    return render_template('home.html')
